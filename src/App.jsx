3968acae72aa0d4635dd02de415af655034b236c
botones de redes <gonzarobledo96@gmail,com>
2023-12-22 12:36:12 -0300

    Primer committ

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
+
 import Navbar from './assets/components/pagina-principal/Navbar';
 import Cartelpy from './assets/components/cartel-pedidos-ya/cartelpy';
 import MenuHorarios from './assets/components/Menu-y-horarios/MenuYhorarios';
