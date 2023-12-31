6c6dc5c7d09f417e3588867d753e652609c926d9
botones de redes <gonzarobledo96@gmail,com>
2023-12-31 17:35:18 -0300

    Primer commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Ajusta esto según el nombre de tu repositorio
+  base: '/Buffet-Stentor/', // Ajusta esto según el nombre de tu repositorio
   plugins: [react()],
 });
